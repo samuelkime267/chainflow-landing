b6169e46d93977ecf779807b18d361de411dc370
samuelkime267 <samuelkime7@gmail.com>
2024-12-01 10:43:55 +0100

    cleaned and refactored code

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,41 @@ import Experience from "./components/Experience";
 import "./style/App.css";
 import { Stats } from "@react-three/drei";
 import { Leva } from "leva";
-// import Hero from "./components/Hero";
-// import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
 
 function App() {
+  const width = 2;
+  const height = 1;
+
   return (
     <main>
       <Leva titleBar={false} collapsed />
       <div className="fixed top-0 left-0 w-full h-full">
-        <Canvas dpr={1} className="w-full h-full">
-          <Stats />
-          <Experience />
-        </Canvas>
+        <div className="relative w-full h-full">
+          <Canvas
+            dpr={1}
+            orthographic
+            camera={{
+              position: [0, 0, 1],
+              top: height / 2,
+              bottom: -height / 2,
+              left: -width / 2,
+              right: width / 2,
+              near: 0.1,
+              far: 1,
+            }}
+          >
+            <Stats />
+            <Experience width={width} height={height} />
+          </Canvas>
+        </div>
       </div>
 
-      {/* <Navbar />
-      <Hero /> */}
+      <div className="relative z-10">
+        <Navbar />
+        <Hero />
+      </div>
     </main>
   );
 }
--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -7,25 +7,17 @@ import useControl from "../hooks/useControl";
 import { ShaderMaterial } from "three";
 import { OrthographicCamera } from "@react-three/drei";
 
-const Experience = () => {
-  const width = 2;
-  const height = 1;
+type experienceProps = {
+  width: number;
+  height: number;
+};
+
+const Experience = ({ width, height }: experienceProps) => {
   const shaderMaterialRef = useRef<ShaderMaterial>(null);
   const uniforms = useControl(shaderMaterialRef);
 
   return (
     <>
-      <OrthographicCamera
-        makeDefault
-        position={[0, 0, 1]}
-        top={height / 2}
-        bottom={-height / 2}
-        left={-width / 2}
-        right={width / 2}
-        near={0.1}
-        far={1}
-      />
-
       <mesh>
         <shaderMaterial
           ref={shaderMaterialRef}
